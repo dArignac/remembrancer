cbde08853afa35a7a2474e6442c912a7d245c946
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2022-07-05 17:02:15 +0200

    feat: intial tauri+vite+svelte code, ci setup and basic countdown

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import Countdown from "./lib/Countdown.svelte";
+</script>
+
+<main>
+  <h1>Remembrancer</h1>
+  <Countdown />
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 0em;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 2rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 1rem auto;
+    max-width: 14rem;
+  }
+</style>
---
+++ src/lib/Countdown.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  $: minutes = 0;
+  $: seconds = 0;
+  let interval;
+
+  function startTimer(duration) {
+    let timer = duration;
+    interval = setInterval(() => {
+      minutes = parseInt("" + timer / 60, 10);
+      seconds = parseInt("" + (timer % 60), 10);
+
+      if (--timer < 0) {
+        timer = duration;
+      }
+    }, 1000);
+  }
+
+  onMount(() => {
+    startTimer(60 * 60);
+    () => {
+      clearInterval(interval);
+    };
+  });
+</script>
+
+<div>
+  {minutes > 9 ? minutes : "0" + minutes}:{seconds > 9
+    ? seconds
+    : "0" + seconds}
+</div>
+
+<style>
+  div {
+    font-size: 2em;
+  }
+</style>
