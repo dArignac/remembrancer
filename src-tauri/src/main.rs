a9f6f00bf449f6037df32c40de9015e8994893bc
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2022-07-06 16:47:05 +0200

    feat: added a tray icon

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,47 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
+use tauri::Manager;
+use tauri::{CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem};
 
 fn main() {
-  let context = tauri::generate_context!();
-  tauri::Builder::default()
-    .menu(if cfg!(target_os = "macos") {
-      tauri::Menu::os_default(&context.package_info().name)
-    } else {
-      tauri::Menu::default()
-    })
-    .run(context)
-    .expect("error while running tauri application");
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+    let show = CustomMenuItem::new("show".to_string(), "Show");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(quit)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(hide)
+        .add_item(show);
+
+    let system_tray = SystemTray::new().with_menu(tray_menu);
+
+    let context = tauri::generate_context!();
+    tauri::Builder::default()
+        .system_tray(system_tray)
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "quit" => {
+                    std::process::exit(0);
+                }
+                "hide" => {
+                    let window = app.get_window("main").unwrap();
+                    window.hide().unwrap();
+                }
+                "show" => {
+                    let window = app.get_window("main").unwrap();
+                    window.show().unwrap()
+                }
+                _ => {}
+            },
+            _ => {}
+        })
+        .menu(if cfg!(target_os = "macos") {
+            tauri::Menu::os_default(&context.package_info().name)
+        } else {
+            tauri::Menu::default()
+        })
+        .run(context)
+        .expect("error while running tauri application");
 }
