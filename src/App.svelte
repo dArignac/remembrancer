51f644407f9f40f856def622bf65db7f3a9034e9
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2022-07-06 17:12:39 +0200

    feat: we can trigger notifications

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,25 @@
 <script lang="ts">
   import Countdown from "./lib/Countdown.svelte";
+  import { isPermissionGranted } from "@tauri-apps/api/notification";
+  import NotificationButton from "./lib/NotificationButton.svelte";
+
+  let notifPromise = isPermissionGranted();
 </script>
 
 <main>
   <h1>Remembrancer</h1>
   <Countdown />
+  <hr />
+  Notification Permissions:
+  {#await notifPromise}
+    loading...
+  {:then notifPromise}
+    {notifPromise}
+  {:catch error}
+    failed: {error.message}
+  {/await}
+  <br /><br />
+  <NotificationButton />
 </main>
 
 <style>
---
+++ src/lib/NotificationButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { sendNotification } from "@tauri-apps/api/notification";
+
+  function onClick() {
+    sendNotification({
+      title: "REMEMBRANCER!!!",
+      //   icon: "icons/icon.png", unclear which value is needed
+      body: "Remembrancer is awesome!",
+    });
+  }
+</script>
+
+<button on:click={onClick}>Trigger Notification</button>
