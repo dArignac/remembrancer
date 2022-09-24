a74c5e8c4ae5136f4f24f4430f60137f281390e3
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2022-09-24 16:09:21 +0200

    fix: fixed signature path #7

--- ci/release.sh
+++ ci/release.sh
@@ -8,7 +8,3 @@ sed -i 's/^    "version": ".*"$/    "version": "'"${VERSION}"'"/g' src-tauri/tau
 sed -i 's/^  "version": ".*",$/  "version": "'"${VERSION}"'",/g' package.json
 sed -i 's/^  "version": ".*",$/  "version": "'"${VERSION}"'",/g' package-lock.json
 sed -i 's/Version: <span>.*<\/span>/Version: <span>'"${VERSION}"'<\/span>/g' src/lib/Version.svelte
-sed -i 's/"version": "v.*",/"version": "v'"${VERSION}"'",/g' updater.json
-sed -i 's/download\/v.*\/remembrancer/download\/v'"${VERSION}"'\/remembrancer/g' updater.json
-sed -i 's/remembrancer_\([0-9]\+\.[0-9]\+\.[0-9]\+\)/remembrancer_'"${VERSION}"'/g' updater.json
-sed -i 's/"pub_date": ".*",/"pub_date": "'"${DATE}"'",/' updater.json
