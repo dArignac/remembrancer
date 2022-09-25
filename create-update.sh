31c768282b2d7fbb2219ef7c1461101f619b201f
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2022-09-25 19:42:42 +0200

    updated script to handle different os #7

--- create-update.sh
+++ create-update.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 VERSION=`echo "$1" | cut -c2-`
 SIGNATURE_LINUX_FILE_DIR=$2
+SIGNATURE_MACOS_FILE_DIR=$3
+SIGNATURE_WINDOWS_FILE_DIR=$4
 
 # restored from github artifacts
 SIGNATURE_LINUX=`cat ${SIGNATURE_LINUX_FILE_DIR}/remembrancer_*_amd64.AppImage.tar.gz.sig`
-# FIXME add Mac and Windows
+SIGNATURE_MACOS=`cat ${SIGNATURE_MACOS_FILE_DIR}/*.app.tar.gz.sig`
+SIGNATURE_WINDOWS=`cat ${SIGNATURE_QINDOWS_FILE_DIR}/remembrancer_*_x64_en-US.msi.zip.sig`
 DATE=`date -u '+%Y-%m-%dT%H:%M:%SZ'`
 
 tee update.json <<EOF
@@ -14,7 +17,7 @@ tee update.json <<EOF
   "pub_date": "${DATE}",
   "platforms": {
     "darwin-x86_64": {
-      "signature": "",
+      "signature": "${SIGNATURE_MACOS}",
       "url": "https://github.com/dArignac/remembrancer/releases/download/v${VERSION}/remembrancer.app.tar.gz"
     },
     "linux-x86_64": {
@@ -22,7 +25,7 @@ tee update.json <<EOF
       "url": "https://github.com/dArignac/remembrancer/releases/download/v${VERSION}/remembrancer_${VERSION}_amd64.AppImage.tar.gz"
     },
     "windows-x86_64": {
-      "signature": "",
+      "signature": "${SIGNATURE_WINDOWS}",
       "url": "https://github.com/dArignac/remembrancer/releases/download/v${VERSION}/remembrancer_${VERSION}_x64_en-US.msi.zip"
     }
   }
